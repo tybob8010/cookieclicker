c611157c2861ed1188c32579c6496fab280b53b8
ぼぶ <tybob8010@gmail.com>
2026-02-03 13:14:02 +0900

    mod1一部変更

--- MODs/mod1.js
+++ MODs/mod1.js
@@ -4,18 +4,7 @@ Game.registerMod('mod1', {
         Game.Notify('Test Mod Loaded!', 'ようこそ、テストMODが有効になりました', '', 3);
 
         // perfectCookie.png を perfectoCookie2.png に置き換える
-        for (var i in Game.Objects){
-            var obj = Game.Objects[i];
-            if (obj.icon === 'perfectCookie.png') obj.icon = 'perfectoCookie2.png';
-        }
-
-        // もし画像パスを直接参照する関数があれば、それも置き換える
-        if (Game.Upgrade) {
-            for (var i in Game.Upgrades){
-                var upg = Game.Upgrades[i];
-                if (upg.icon === 'perfectCookie.png') upg.icon = 'perfectoCookie2.png';
-            }
-        }
+        Game.Loader.Replace('perfectCookie.png','perfectCookie2.png');
     },
     save: function(){ return ''; },
     load: function(str){ }
