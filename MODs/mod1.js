469c33870858a60b767637317ae36665336a1ea1
ぼぶ <tybob8010@gmail.com>
2026-02-03 13:34:19 +0900

    mod1一部変更(惜しい)

--- MODs/mod1.js
+++ MODs/mod1.js
@@ -1,17 +1,17 @@
 Game.registerMod('mod1', {
     init: function() {
         // 読み込まれた時にメッセージを出す
-        Game.Notify('Test Mod Loaded!', 'ようこそ、テストMODが有効になりました', '', 3);
+        Game.Notify('mod1 Loaded!', 'ようこそ mod1 が有効になりました', '', 3);
 
         // perfectCookie.png を perfectoCookie2.png に置き換える
         if (Game.Loader) {
-            Game.Loader.Replace('perfectCookie.png', 'perfectoCookie2.png');
+            Game.Loader.Replace('perfectCookie.png', 'perfectoCookie 2.png');
         }
 
         // ゲーム初期化後にも置き換えを保証
         Game.registerHook('reset', function() {
             if (Game.Loader) {
-                Game.Loader.Replace('perfectCookie.png', 'perfectoCookie2.png');
+                Game.Loader.Replace('perfectCookie.png', 'perfectoCookie 2.png');
             }
         });
     },
